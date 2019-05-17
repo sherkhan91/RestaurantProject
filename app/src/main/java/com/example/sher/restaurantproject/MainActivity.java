16ecaee18eecb7b04645219f8bbf17396d23b121
sherkhan <sher9khan@gmail.com>
2019-05-17 23:24:58 +0500

    reusable toolbar has been created

--- app/src/main/java/com/example/sher/restaurantproject/MainActivity.java
+++ app/src/main/java/com/example/sher/restaurantproject/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.sher.restaurantproject;
 
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        // getting the action bar and putton an icon on actionbar
+//        ActionBar actionBar =  getSupportActionBar();
+//        actionBar.setDisplayShowHomeEnabled(true);
+//        actionBar.setIcon(R.mipmap.ic_launcher);
+//
+//
+//        TextView actionbarTitle = new TextView(getApplicationContext());
+//        ViewGroup.LayoutParams lp = new RelativeLayout.LayoutParams(
+//                ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT
+//        );
+//
+//        actionbarTitle.setLayoutParams(lp);
+//        actionbarTitle.setText(actionBar.getTitle());
+//        actionbarTitle.setTextColor(Color.BLACK);
+//        actionbarTitle.setGravity(Gravity.CENTER);
+//        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+//
+//        actionBar.setCustomView(actionbarTitle);
+
     }
 }
