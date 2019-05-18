52aeab4222c4cd9b8801c2d950da68fada5a94ed
sherkhan <sher9khan@gmail.com>
2019-05-19 00:32:37 +0500

    dummy User interface with recycler view is ready

--- app/src/main/java/com/example/sher/restaurantproject/MainActivity.java
+++ app/src/main/java/com/example/sher/restaurantproject/MainActivity.java
@@ -4,38 +4,51 @@ import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
+    private List<String> RestaurantList = new ArrayList<>();
+    private List<String> locationList = new ArrayList<>();
+    private List<String> countryList = new ArrayList<>();
+    private List<String> ratingList = new ArrayList<>();
+    private List<String> reviewCountList = new ArrayList<>();
+
+    private List<String> ImageList = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private MyRecyclerViewAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        // getting the action bar and putton an icon on actionbar
-//        ActionBar actionBar =  getSupportActionBar();
-//        actionBar.setDisplayShowHomeEnabled(true);
-//        actionBar.setIcon(R.mipmap.ic_launcher);
-//
-//
-//        TextView actionbarTitle = new TextView(getApplicationContext());
-//        ViewGroup.LayoutParams lp = new RelativeLayout.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT
-//        );
-//
-//        actionbarTitle.setLayoutParams(lp);
-//        actionbarTitle.setText(actionBar.getTitle());
-//        actionbarTitle.setTextColor(Color.BLACK);
-//        actionbarTitle.setGravity(Gravity.CENTER);
-//        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-//
-//        actionBar.setCustomView(actionbarTitle);
+
+        for(int i=0;i<20;i++){
+            RestaurantList.add("Restaurant: "+String.valueOf(i));
+            locationList.add("\uD83D\uDCCC Location: "+String.valueOf(i));
+            countryList.add("Country: "+String.valueOf(i));
+            ratingList.add(String.valueOf(4));
+            reviewCountList.add(String.valueOf(i)+"Reviews");
+            ImageList.add("ImageURL"+String.valueOf(i));
+
+
+        }
+
+
+        // set up the recycler view
+        RecyclerView recyclerView = findViewById(R.id.main_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new MyRecyclerViewAdapter(this,RestaurantList,locationList,countryList,ratingList,reviewCountList,ImageList);
+        recyclerView.setAdapter(mAdapter);
 
     }
+
+//    @Override
+//    public void onItemClick(View view, int position){
+//        Toast.makeText(this,"You clicked", Toast.LENGTH_LONG).show();
+//    }
 }
---
+++ app/src/main/java/com/example/sher/restaurantproject/MyRecyclerViewAdapter.java
@@ -0,0 +1,104 @@
+package com.example.sher.restaurantproject;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder> {
+
+    private List<String> restaurant_name;
+    private List<String> location;
+    private List<String> country_names;
+    private List<String> ratingBar_number;
+    private List<String> review_count;
+    private List<String> image_names;
+
+
+
+    private LayoutInflater mInflater;
+    private AdapterView.OnItemClickListener mClickListener;
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView template_restaurantText;
+        public TextView template_locationText;
+        public TextView template_restaurant_country;
+        public RatingBar template_ratingBar;
+        public TextView template_reviewsCount;
+        public ImageView templateImageView;
+
+
+
+
+
+        public MyViewHolder(View view){
+            super(view);
+
+            template_restaurantText = (TextView) view.findViewById(R.id.template_restaurant_name);
+            template_locationText = (TextView) view.findViewById(R.id.template_location_text);
+            template_restaurant_country = (TextView) view.findViewById(R.id.template_restaurant_country);
+            template_ratingBar = (RatingBar) view.findViewById(R.id.template_star_review);
+            template_reviewsCount = (TextView) view.findViewById(R.id.template_review_count);
+            templateImageView = (ImageView) view.findViewById(R.id.template_image);
+
+        }
+
+    }
+
+
+    // data is passed into constructor
+    public MyRecyclerViewAdapter(Context context,List<String> restaurant_name,List<String> location_name,List<String> restaurant_country,List<String> rating_count, List<String> reviews_count,List<String> image_name){
+        this.mInflater = LayoutInflater.from(context);
+        this.restaurant_name = restaurant_name;
+        this.location = location_name;
+        this.country_names = restaurant_country;
+        this.ratingBar_number = rating_count;
+        this.review_count = reviews_count;
+        this.image_names = image_name;
+        //this.mData = data;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view =  mInflater.inflate(R.layout.recyclerview_row,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position){
+
+        String loc_name = location.get(position);
+        String rt_name = restaurant_name.get(position);
+        String im_name = image_names.get(position);
+
+        holder.template_restaurantText.setText(restaurant_name.get(position));
+        holder.template_locationText.setText(location.get(position));
+        holder.template_restaurant_country.setText(country_names.get(position));
+        holder.template_ratingBar.setNumStars(Integer.parseInt(ratingBar_number.get(position)));
+        holder.template_reviewsCount.setText(review_count.get(position));
+        holder.templateImageView.setImageResource(R.drawable.restaurant_pic);
+       // holder.template_locationText.setText(loc_name);
+
+
+
+
+       // String textViewDetail =  mData.get(position);
+        //holder.rowText.setText(textViewDetail);
+    }
+
+    @Override
+    public int getItemCount(){
+
+        return location.size();
+    }
+
+}
